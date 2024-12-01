cc041ad96ea59773e176984bdab439425c24b01e
vetterfl <florian@vetter.is>
2024-12-01 15:46:42 +0100

    Day 1 Part two and chaged directory structure.

--- day_1/main.c
+++ day_1/main.c
@@ -24,7 +24,7 @@ void bubbleSort(int arr[], int n) {
 
 int main() {
 
-  printf("Hello AoC Day 1\n");
+  printf("Hello AoC Day 1\n\n");
 
   int len = sizeof(left) / sizeof(left[0]);
   // sort left numbers
@@ -36,10 +36,25 @@ int main() {
   // print numbers side by side
   for (int i = 0; i < len; i++) {
     sum += abs(left[i]-right[i]);
-    printf("%d\t%d distance: %d\n", left[i], right[i], abs(left[i] - right[i]) );
+    //printf("%d\t%d distance: %d\n", left[i], right[i], abs(left[i] - right[i]) );
   }
 
-  printf("Sum: %d\n", sum);
+  printf("Sum of the differences: %d\n", sum);
+
+  printf("Now calculating the similarity score:\n");
+  sum = 0;
+  for (int i = 0; i < len; i++) {
+    int count = 0;  
+    for (int j = 0; j < len; j++) {
+      if (right[j] == left[i]) count++;
+    }
+    //printf("%d\t count:%d\n", left[i], count);
+    sum += left[i] * count;
+  }
+
+  printf("Similarity score: %d\n", sum);
+
+
 
   return 0;
 }
